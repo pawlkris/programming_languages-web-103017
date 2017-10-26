14c23f82ffafb8aa27763a4f4ba529de1828fb3a
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-26 14:47:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,6 +5,7 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
   languages[:oo].each do |language,hash|
-    new_hash[language] => {hash}
+    new_hash[language]
   end
+  binding.pry
 end
