754797cce9f8f996635da4953a057015954df187
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-26 14:44:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require "pry"
+
+def add_language(hash, language, stylearray)
+  return hash[:language][:style] => stylearray
+end
+
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages[:oo].each do |language,hash|
+    binding.pry
+    new_hash[language] => hash
+    
+  
 end
